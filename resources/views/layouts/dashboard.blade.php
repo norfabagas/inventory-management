7a6c4b8d6d69f941e0ef7fd3b3ce7c0016576a34
Norfa Bagas <bgsnorfa@gmail.com>
2018-07-20 00:33:49 +0700

    dashboard menu

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -47,7 +47,7 @@
     <footer class="sticky-footer">
       <div class="container">
         <div class="text-center">
-          <small>Copyright © Your Website 2018</small>
+          <small>Copyright © 2018</small>
         </div>
       </div>
     </footer>
