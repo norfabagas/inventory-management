276cf7a8f8fdf39d301ae33349dbe13fbbee3c55
Norfa Bagas <bgsnorfa@gmail.com>
2018-08-01 11:27:53 +0700

    mail config

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ Route::get('/register', function () {
   return redirect('login');
 });
 Route::get('/logout', 'Auth\LoginController@logout');
-
+Route::get('/home', function () {
+  return redirect('dashboard');
+});
 // Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', function () {
